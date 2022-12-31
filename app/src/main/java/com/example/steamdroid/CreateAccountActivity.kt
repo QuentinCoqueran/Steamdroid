d5e7aaa4be93a5a42b4012a4c8291246780f9924
David <zdavid1@myges.fr>
2022-12-31 19:32:17 +0100

    register with verification

--- app/src/main/java/com/example/steamdroid/CreateAccountActivity.kt
+++ app/src/main/java/com/example/steamdroid/CreateAccountActivity.kt
@@ -1,19 +1,17 @@
 package com.example.steamdroid
 
-import android.util.Log
-import android.widget.Toast
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
-import com.google.android.gms.tasks.Task
+import android.util.Patterns
+import android.widget.Button
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.QuerySnapshot
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class CreateAccountActivity : Activity() {
@@ -24,29 +22,113 @@ class CreateAccountActivity : Activity() {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_account)
-        val emailInput = findViewById<EditText>(R.id.email_input)
-        val passwordInput = findViewById<TextView>(R.id.password_input)
-        val createAccount = findViewById<TextView>(R.id.create_account)
-        val logInRedirect = findViewById<TextView>(R.id.log_in_redirect)
+        val emailInput = findViewById<TextInputEditText>(R.id.email_input)
+        val passwordInput = findViewById<TextInputEditText>(R.id.password_input)
+        val usernameInput = findViewById<TextInputEditText>(R.id.username_input)
+        val createAccount = findViewById<Button>(R.id.create_account)
+        val logInRedirect = findViewById<Button>(R.id.log_in_redirect)
+        val validatePassword = findViewById<TextInputEditText>(R.id.password_confirm_label)
+        var checkPass = false
         createAccount.setOnClickListener {
             val email = emailInput.text.toString()
             val password = passwordInput.text.toString()
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                val create = createAccountWithUsername(email, password);
-                if (create != null) {
-                    if (create.isSuccessful) {
-                        createAccount(email, password)
-                    } else {
-                        Toast.makeText(this, "Account not created", Toast.LENGTH_SHORT).show()
-                    }
-                }
+            val username = usernameInput.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty() && username.isNotEmpty() && checkPass) {
+                createAccountWithUsername(emailInput, passwordInput, usernameInput)
             } else {
-                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                if (email.isEmpty()) {
+                    emailInput.setBackgroundResource(R.drawable.border_red)
+                    emailInput.error = resources.getString(R.string.email_error_required)
+                }
+                if (password.isEmpty()) {
+                    passwordInput.setBackgroundResource(R.drawable.border_red)
+                    passwordInput.error = resources.getString(R.string.password_error_required)
+                }
+                if (username.isEmpty()) {
+                    usernameInput.setBackgroundResource(R.drawable.border_red)
+                    usernameInput.error = resources.getString(R.string.username_error_required)
+                }
             }
         }
+
+        //redirect to log in page
         logInRedirect.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
         }
+
+        usernameInput.doOnTextChanged { text, _, _, _ ->
+            val db = FirebaseFirestore.getInstance()
+            val docRef = db.collection("steamandroid").whereEqualTo("username", text.toString())
+            docRef.get().addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    usernameInput.setBackgroundResource(R.drawable.border_red)
+                    usernameInput.error = resources.getString(R.string.username_error)
+                    checkPass = false
+                } else {
+                    usernameInput.setBackgroundResource(R.drawable.input_border_rounded)
+                    usernameInput.error = null
+                    checkPass = true
+                }
+            }
+        }
+        emailInput.doOnTextChanged { text, _, _, _ ->
+            val db = FirebaseFirestore.getInstance()
+            val docRef = db.collection("steamandroid").whereEqualTo("email", text.toString())
+            docRef.get().addOnSuccessListener { documents ->
+                if (!Patterns.EMAIL_ADDRESS.matcher(text.toString())
+                        .matches()
+                ) {
+                    emailInput.setBackgroundResource(R.drawable.border_red)
+                    checkPass = false
+                    emailInput.error = resources.getString(R.string.email_error_regex)
+                } else if (!documents.isEmpty) {
+                    emailInput.setBackgroundResource(R.drawable.border_red)
+                    checkPass = false
+                    emailInput.error = resources.getString(R.string.email_error)
+                } else {
+                    emailInput.setBackgroundResource(R.drawable.input_border_rounded)
+                    emailInput.error = null
+                    checkPass = true
+                }
+            }
+        }
+        // check if validatePassword is same of passwordInput
+        validatePassword.doOnTextChanged() { _, _, _, _ ->
+            validatePassword.setBackgroundResource(R.drawable.input_border_rounded)
+            validatePassword.error = null
+            checkPass = false
+            if (passwordInput.text.toString() != validatePassword.text.toString()) {
+                validatePassword.setBackgroundResource(R.drawable.border_red)
+                validatePassword.error = resources.getString(R.string.password_error)
+                checkPass = false
+            } else {
+                validatePassword.setBackgroundResource(R.drawable.input_border_rounded)
+                validatePassword.error = null
+                checkPass = true
+            }
+        }
+        // check if validatePassword is same of passwordInput
+        passwordInput.doOnTextChanged() { _, _, _, _ ->
+            passwordInput.setBackgroundResource(R.drawable.input_border_rounded)
+            passwordInput.error = null
+            checkPass = false
+            if (passwordInput.text.toString().isNotEmpty()) {
+                if (passwordInput.text.toString() != validatePassword.text.toString()) {
+                    validatePassword.setBackgroundResource(R.drawable.border_red)
+                    validatePassword.error = resources.getString(R.string.password_error)
+                    checkPass = false
+                } else {
+                    validatePassword.setBackgroundResource(R.drawable.input_border_rounded)
+                    validatePassword.error = null
+                    checkPass = true
+                }
+            } else {
+                validatePassword.setBackgroundResource(R.drawable.input_border_rounded)
+                validatePassword.error = null
+                checkPass = false
+            }
+        }
+
         auth = Firebase.auth
     }
 
@@ -63,68 +145,38 @@ class CreateAccountActivity : Activity() {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
-                    createAccountWithUsername(email, password)
                 } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
                     updateUI(null)
                 }
             }
     }
 
 
-    private fun createAccountWithUsername(email: String, password: String): Task<QuerySnapshot>? {
-        val usernameInput = findViewById<TextView>(R.id.username_input)
+    private fun createAccountWithUsername(
+        emailInput: TextInputEditText,
+        passwordInput: TextInputEditText,
+        usernameInput: TextInputEditText
+    ) {
         val username = usernameInput.text.toString()
-        if (username.isNotEmpty()) {
-            val user = hashMapOf(
-                "username" to username,
-                "email" to email,
-                "password" to password
-            )
-            val db = Firebase.firestore
-            return db.collection("steamandroid")
-                .whereEqualTo("username", username)
-                .whereEqualTo("email", email)
-                .get()
-                .addOnSuccessListener { documents ->
-                    if (documents.isEmpty) {
-                        db.collection("steamandroid")
-                            .add(user)
-                            .addOnSuccessListener { documentReference ->
-                                Log.d(
-                                    TAG,
-                                    "DocumentSnapshot added with ID: ${documentReference.id}"
-                                )
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w(TAG, "Error adding document", e)
-                            }
-                    } else {
-                        Log.w(TAG, "Username already taken.")
-                        Toast.makeText(
-                            baseContext, "Username already taken.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+        val email = emailInput.text.toString()
+        val password = passwordInput.text.toString()
+        val user = hashMapOf(
+            "username" to username,
+            "email" to email,
+            "password" to password
+        )
+        val db = FirebaseFirestore.getInstance()
+        db.collection("steamandroid").add(user).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                createAccount(email, password)
+            } else {
+                updateUI(null)
+            }
         }
-        return null
-    }
-
-    companion object {
-        private const val TAG = "EmailPassword"
     }
 }
-
 
 private fun updateUI(user: FirebaseUser?) {
 
