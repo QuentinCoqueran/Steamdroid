bcc46e566c99d24eeeecd17e386e3648eca6411b
QuentinCoqueran <coqueranquentin@gmail.com>
2023-01-31 16:47:51 +0100

    id + stars gamereviex recyclerView

--- app/src/main/java/com/example/steamdroid/recycler/GameReviewViewHolder.kt
+++ app/src/main/java/com/example/steamdroid/recycler/GameReviewViewHolder.kt
@@ -2,21 +2,19 @@ package com.example.steamdroid.recycler
 
 import android.annotation.SuppressLint
 import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.steamdroid.R
-import com.example.steamdroid.model.Product
+import com.example.steamdroid.model.GameReview
 
 class GameReviewViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
-    /*private val author = v.findViewById<TextView>(R.id.author)
-    private val vote = v.findViewById<TextView>(R.id.vote)*/
-    private val review = v.findViewById<TextView>(R.id.review)
+    private val author = v.findViewById<TextView>(R.id.author)
+    private val reviewText = v.findViewById<TextView>(R.id.review_text)
 
     @SuppressLint("SetTextI18n")
-    fun updateView(product: Product) {
-
+    fun updateView(review: GameReview) {
+        author.text = review.author
+        reviewText.text = review.review
     }
 }
--- app/src/main/java/com/example/steamdroid/recycler/ProductAdapter.kt
+++ app/src/main/java/com/example/steamdroid/recycler/ProductAdapter.kt
@@ -7,20 +7,20 @@ import com.example.steamdroid.R
 import com.example.steamdroid.model.Product
 
 class ProductAdapter(private var products : List<Product>) : RecyclerView.Adapter<ProductViewHolder>() {
-        override fun getItemCount(): Int = products.size
+    override fun getItemCount(): Int = products.size
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-            return ProductViewHolder(
-                LayoutInflater.from(parent.context)
-                    .inflate(R.layout.list_item, parent, false)
-            )
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item, parent, false)
+        )
+    }
 
-        override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-            holder.updateView(products[position])
-        }
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.updateView(products[position])
+    }
 
-        fun updateProducts(newProducts: List<Product>) {
-            products = products.plus(newProducts)
-        }
+    fun updateProducts(newProducts: List<Product>) {
+        products = products.plus(newProducts)
+    }
 }
