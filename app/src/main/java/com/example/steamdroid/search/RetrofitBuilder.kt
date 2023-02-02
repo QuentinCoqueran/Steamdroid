3a78f5f4f03f36cd66cec04ae665de5bd7156351
david <zerbib.david.1@gmail.com>
2023-02-02 17:47:41 +0100

    add coroutine bestsellers

--- app/src/main/java/com/example/steamdroid/search/RetrofitBuilder.kt
+++ app/src/main/java/com/example/steamdroid/search/RetrofitBuilder.kt
@@ -1,22 +1,30 @@
 import com.example.steamdroid.SteamApi
+import com.example.steamdroid.home.BestSellersResponse
 import com.example.steamdroid.model.Game
 import com.example.steamdroid.model.GameTypeAdapter
 import com.example.steamdroid.search.SearchGame
 import com.example.steamdroid.search.SearchGameTypeAdapter
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitBuilder {
 
     private fun getSearchGameRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.steampowered.com")
-            .addConverterFactory(GsonConverterFactory.create(
-                GsonBuilder()
-                .registerTypeAdapter(List::class.java, SearchGameTypeAdapter())
-                .create()))
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder()
+                        .registerTypeAdapter(List::class.java, SearchGameTypeAdapter())
+                        .create()
+                )
+            )
             .addCallAdapterFactory(CoroutineCallAdapterFactory()) // Add this line
             .build()
     }
@@ -24,15 +32,25 @@ object RetrofitBuilder {
     private fun getGameDetailsRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://store.steampowered.com")
-            .addConverterFactory(GsonConverterFactory.create(
-                GsonBuilder()
-                    .registerTypeAdapter(Game::class.java, GameTypeAdapter())
-                    .create()))
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder()
+                        .registerTypeAdapter(Game::class.java, GameTypeAdapter())
+                        .create()
+                )
+            )
             .addCallAdapterFactory(CoroutineCallAdapterFactory()) // Add this line
+            .build()
+    }
+
+    private fun getBestsellersRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.steampowered.com")
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 
     val searchGameService: SteamApi = getSearchGameRetrofit().create(SteamApi::class.java)
     val gameDetailsService: SteamApi = getGameDetailsRetrofit().create(SteamApi::class.java)
-
+    val bestsellersService: SteamApi = getBestsellersRetrofit().create(SteamApi::class.java)
 }
